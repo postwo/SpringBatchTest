868896dc5621f1b4756195c5e1003cfbec5099f7
postwo <codo7717@naver.com>
2024-11-29 22:32:07 +0900

    ã세팅

---
+++ src/main/java/com/example/SpringBatchTest/SpringBatchTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBatchTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTestApplication.class, args);
+	}
+
+}
