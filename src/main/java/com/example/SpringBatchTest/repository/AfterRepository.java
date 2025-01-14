1a7f9ae437125dc45797ad05276b9f5b0e925c2a
postwo <codo7717@naver.com>
2025-01-14 14:30:06 +0900

    테이블 to 테이블

---
+++ src/main/java/com/example/SpringBatchTest/repository/AfterRepository.java
@@ -0,0 +1,8 @@
+package com.example.SpringBatchTest.repository;
+
+import com.example.SpringBatchTest.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+
+}
---
+++ src/main/java/com/example/SpringBatchTest/repository/BeforeRepository.java
@@ -0,0 +1,8 @@
+package com.example.SpringBatchTest.repository;
+
+import com.example.SpringBatchTest.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity,Long> {
+
+}
